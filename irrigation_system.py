659f4e7aace3b3874d81adaf72116727ada70a3f
Jeremiah Larry King Ungsod <67980301+Jeremiah019@users.noreply.github.com>
2022-10-28 04:57:36 +0800

    remove

--- irrigation_system.py
+++ irrigation_system.py
@@ -45,9 +45,7 @@ class tkinterApp(tk.Tk):
 		frame.tkraise()
 
 # first window frame startpage
-class Loading(tk.Frame):
-    global label2
-    
+class Loading(tk.Frame):  
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
@@ -68,65 +66,62 @@ class Loading(tk.Frame):
         
 # second window frame page1
 class StartPage(tk.Frame):
-
-        def __init__(self, parent, controller):
-
-                tk.Frame.__init__(self, parent)
-
-                # label of frame Layout 2
-                label = ttk.Label(self, text ="Automatic Irrigation System", font = "-size 16")
-                # putting the grid in its place by using
-                # grid
-                label.place(anchor = "center", relx = 0.5, rely = 0.20)
+    
+    def __init__(self, parent, controller):
+        
+        tk.Frame.__init__(self, parent)
+        
+        label = ttk.Label(self, text ="Automatic Irrigation System", font = "-size 16")
+        label.place(anchor = "center", relx = 0.5, rely = 0.20)
                 
-                label2 = ttk.Label(self)
-                label2.place(anchor = "center", relx = 0.5, rely = 0.90)
+        label2 = ttk.Label(self)
+        label2.place(anchor = "center", relx = 0.5, rely = 0.90)
 
-                btn_labels_1 = ttk.Label(master=label2, bootstyle = "sucess", text = "System is running...", width = 20)
-                btn_labels_1.place(relx=0.25, rely=0.50, anchor='center')
+        # btn_labels_1 = ttk.Label(master=label2, bootstyle = "sucess", text = "System is running...", width = 20)
+        # btn_labels_1.place(relx=0.25, rely=0.50, anchor='center')
 
-                progress = ttk.Progressbar(label2, bootstyle = "striped", orient='horizontal', length=200, mode='determinate')
-                progress.place(relx=0.50, rely=0.50, anchor='center')
+        # progress = ttk.Progressbar(label2, bootstyle = "striped", orient='horizontal', length=200, mode='determinate')
+        # progress.place(relx=0.50, rely=0.50, anchor='center')
 
-                import time
+        # import time
 
-                progress['value'] = 20
-                label2.update_idletasks()
-                time.sleep(1)
+        # progress['value'] = 20
+        # label2.update_idletasks()
+        # time.sleep(1)
 
-                progress['value'] = 40
-                label2.update_idletasks()
-                time.sleep(1)
+        # progress['value'] = 40
+        # label2.update_idletasks()
+        # time.sleep(1)
 
-                progress['value'] = 50
-                label2.update_idletasks()
-                time.sleep(1)
+        # progress['value'] = 50
+        # label2.update_idletasks()
+        # time.sleep(1)
 
-                progress['value'] = 60
-                self.update_idletasks()
-                time.sleep(1)
+        # progress['value'] = 60
+        # self.update_idletasks()
+        # time.sleep(1)
 
-                progress['value'] = 80
-                label2.update_idletasks()
-                time.sleep(1)
+        # progress['value'] = 80
+        # label2.update_idletasks()
+        # time.sleep(1)
 
-                progress['value'] = 100
-                label2.update_idletasks()
-                btn_labels_1.destroy()
-                progress.destroy()
+        # progress['value'] = 100
+        # label2.update_idletasks()
+        # btn_labels_1.destroy()
+        # progress.destroy()
 
-                label2 = ttk.Label(self)
-                label2.place(anchor = "center", relx = 0.5, rely = 0.50)
+        label2 = ttk.Label(self)
+        label2.place(anchor = "center", relx = 0.5, rely = 0.50)
 
-                button1 = customtkinter.CTkButton(label2, text ="Temperature", command = lambda : controller.show_frame(Page1))
-                button2 = customtkinter.CTkButton(label2, text ="Soil Moisture", command = lambda : controller.show_frame(Page2))
-                button3 = customtkinter.CTkButton(label2, text ="Humidity", command = lambda : controller.show_frame(Page3))
-                button4 = customtkinter.CTkButton(label2, text ="Water Level", command = lambda : controller.show_frame(Page4))
+        button1 = customtkinter.CTkButton(label2, text ="Temperature", width=100, height=100,command = lambda : controller.show_frame(Page1))
+        button2 = customtkinter.CTkButton(label2, text ="Soil Moisture", width=100, height=100,command = lambda : controller.show_frame(Page2))
+        button3 = customtkinter.CTkButton(label2, text ="Humidity",width=100, height=100, command = lambda : controller.show_frame(Page3))
+        button4 = customtkinter.CTkButton(label2, text ="Water Level",width=100, height=100, command = lambda : controller.show_frame(Page4))
                 
-                button1.grid(row = 1, column = 1)
-                button2.grid(row = 1, column = 2)
-                button3.grid(row = 2, column = 1)
-                button4.grid(row = 2, column = 2)
+        button1.grid(row = 1, column = 1, padx = 10, pady = 10)
+        button2.grid(row = 1, column = 2, padx = 10, pady = 10)
+        button3.grid(row = 2, column = 1, padx = 10, pady = 10)
+        button4.grid(row = 2, column = 2, padx = 10, pady = 10)
                 
 # second window frame page1
 
@@ -153,9 +148,7 @@ class Page1(tk.Frame):
 		label2 = ttk.Label(self)
 		label2.place(anchor = "center", relx = 0.5, rely = 0.90)
 	
-		# button to show frame 2 with text
-		# layout2
-		button = customtkinter.CTkButton(self, width=40, height=40, corner_radius=40,fg_color="#76BA1B", hover_color="#ACDF87",
+		button = customtkinter.CTkButton(label, width=40, height=40, corner_radius=40, hover_color="#ACDF87",
                                                  text ="StartPage", command = lambda : controller.show_frame(StartPage))
 		button1 = customtkinter.CTkButton(label2, text ="Temperature", command = lambda : controller.show_frame(Page1))
 	
